55d8d543df7aa52b58c3f9edac61f48667243cc4
Your Name <you@example.com>
2026-04-19 19:36:48 +0300

    Update portfolio gallery

---
+++ assets/data/portfolio-data.js
@@ -0,0 +1,163 @@
+window.portfolioItems = [
+  {
+    "file": "portfolio-01.jpg",
+    "alt": "Реализованный проект 1",
+    "orientation": "portrait",
+    "width": 1200,
+    "height": 1800
+  },
+  {
+    "file": "portfolio-02.jpg",
+    "alt": "Реализованный проект 2",
+    "orientation": "landscape",
+    "width": 1600,
+    "height": 1000
+  },
+  {
+    "file": "portfolio-03.jpg",
+    "alt": "Реализованный проект 3",
+    "orientation": "landscape",
+    "width": 1600,
+    "height": 1000
+  },
+  {
+    "file": "portfolio-04.jpg",
+    "alt": "Реализованный проект 4",
+    "orientation": "landscape",
+    "width": 1600,
+    "height": 1000
+  },
+  {
+    "file": "portfolio-05.jpg",
+    "alt": "Реализованный проект 5",
+    "orientation": "portrait",
+    "width": 1200,
+    "height": 1800
+  },
+  {
+    "file": "portfolio-06.jpg",
+    "alt": "Реализованный проект 6",
+    "orientation": "landscape",
+    "width": 1600,
+    "height": 1000
+  },
+  {
+    "file": "portfolio-07.jpg",
+    "alt": "Реализованный проект 7",
+    "orientation": "landscape",
+    "width": 2000,
+    "height": 1500
+  },
+  {
+    "file": "portfolio-08.jpg",
+    "alt": "Реализованный проект 8",
+    "orientation": "landscape",
+    "width": 2000,
+    "height": 1500
+  },
+  {
+    "file": "portfolio-09.jpg",
+    "alt": "Реализованный проект 9",
+    "orientation": "landscape",
+    "width": 1920,
+    "height": 1360
+  },
+  {
+    "file": "portfolio-10.jpg",
+    "alt": "Реализованный проект 10",
+    "orientation": "square",
+    "width": 1920,
+    "height": 1920
+  },
+  {
+    "file": "portfolio-11.png",
+    "alt": "Реализованный проект 11",
+    "orientation": "landscape",
+    "width": 1920,
+    "height": 1360
+  },
+  {
+    "file": "portfolio-12.jpg",
+    "alt": "Реализованный проект 12",
+    "orientation": "square",
+    "width": 1920,
+    "height": 1920
+  },
+  {
+    "file": "portfolio-13.jpg",
+    "alt": "Реализованный проект 13",
+    "orientation": "square",
+    "width": 1920,
+    "height": 1920
+  },
+  {
+    "file": "portfolio-14.jpg",
+    "alt": "Реализованный проект 14",
+    "orientation": "square",
+    "width": 1920,
+    "height": 1920
+  },
+  {
+    "file": "portfolio-15.jpg",
+    "alt": "Реализованный проект 15",
+    "orientation": "square",
+    "width": 1080,
+    "height": 1080
+  },
+  {
+    "file": "portfolio-16.jpg",
+    "alt": "Реализованный проект 16",
+    "orientation": "square",
+    "width": 1024,
+    "height": 1024
+  },
+  {
+    "file": "portfolio-17.jpg",
+    "alt": "Реализованный проект 17",
+    "orientation": "square",
+    "width": 2000,
+    "height": 2000
+  },
+  {
+    "file": "portfolio-18.jpg",
+    "alt": "Реализованный проект 18",
+    "orientation": "square",
+    "width": 2400,
+    "height": 2400
+  },
+  {
+    "file": "portfolio-19.jpg",
+    "alt": "Реализованный проект 19",
+    "orientation": "landscape",
+    "width": 2000,
+    "height": 1500
+  },
+  {
+    "file": "portfolio-20.jpg",
+    "alt": "Реализованный проект 20",
+    "orientation": "landscape",
+    "width": 2000,
+    "height": 1500
+  },
+  {
+    "file": "portfolio-21.jpg",
+    "alt": "Реализованный проект 21",
+    "orientation": "square",
+    "width": 1200,
+    "height": 1200
+  },
+  {
+    "file": "portfolio-22.jpg",
+    "alt": "Реализованный проект 22",
+    "orientation": "square",
+    "width": 1200,
+    "height": 1200
+  },
+  {
+    "file": "portfolio-23.jpg",
+    "alt": "Реализованный проект 23",
+    "orientation": "square",
+    "width": 1200,
+    "height": 1200
+  }
+];
